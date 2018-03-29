a3bd3119f1a5053b4fb2afa46fca2f1dc2a974ac
Dominik Harmim <harmim6@gmail.com>
2018-03-29 01:34:20 +0100

    StockItemTester

---
+++ tests/StockItemTester.java
@@ -0,0 +1,12 @@
+/**
+ * StockItem tester.
+ *
+ * @author Dominik Harmim harmim6@gmail.com
+ */
+public class StockItemTester
+{
+	public static void main(String[] args)
+	{
+		System.out.print(10);
+	}
+}
